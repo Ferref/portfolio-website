b3955a84f4ba7f83c307560e5924c5c014a4f98d
Kovács Ferenc <83652379+Ferref@users.noreply.github.com>
2025-05-29 14:12:50 +0200

    Initial setup with pages

---
+++ laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kovács Ferenc</title>
+</head>
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/style.css'])
+    @stack('scripts')
+<body>
+<header>
+    @include('layouts.navbar')
+</header>
+    <div class="main-container">
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ laravel/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-dark position-fixed w-100">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+    <ul class="navbar-nav d-flex align-items-center">
+      <li class="nav-item active mx-3">
+        <a class="nav-link text-light" href="#">About me</a>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link text-light" href="#">Favourite projects</a>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link text-light" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
