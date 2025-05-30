ff1b27e1212d1a27db7306d2e685449da5b8116c
Kovács Ferenc <83652379+Ferref@users.noreply.github.com>
2025-05-30 11:46:15 +0200

    Navbar fix

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,5 +1,33 @@
-@extends('layouts.app')
-@section('content')
-<div class="home-container d-flex justify-content-center">
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Bootstrap Navbar Test</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="padding-top: 70px;">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#">My Site</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav d-flex align-items-center">
+        <li class="nav-item active mx-3">
+          <a class="nav-link text-light" href="#">About me</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link text-light" href="#">Pet projects</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
--- laravel/resources/views/layouts/navbar.blade.php
+++ laravel/resources/views/layouts/navbar.blade.php
@@ -1,16 +1,20 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-dark position-fixed w-100 z-3">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-    <ul class="navbar-nav d-flex align-items-center">
-      <li class="nav-item active mx-3">
-        <a class="nav-link text-light" href="{{ route('about') }}">About me</a>
-      </li>
-      <li class="nav-item mx-3">
-        <a class="nav-link text-light" href="{{ route('projects') }}">Pet projects</a>
-      </li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav d-flex align-items-center">
+        <li class="nav-item active mx-3">
+          <a class="nav-link text-light" href="{{ route('about') }}">About me</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link text-light" href="{{ route('projects') }}">Pet projects</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
+
