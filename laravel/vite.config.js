cb093674042cacfe2d81b5cdb1710e5352915174
Kovács Ferenc <83652379+Ferref@users.noreply.github.com>
2025-06-05 16:20:15 +0200

    Configured to live

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -5,8 +5,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/style.css'],
+            refresh: process.env.APP_ENV !== 'production',
         }),
         // tailwindcss(),
     ],
