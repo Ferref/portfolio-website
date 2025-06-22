8404d5947731c399cb12de1a500dffe11760fa08
Kovács Ferenc <83652379+Ferref@users.noreply.github.com>
2025-06-22 20:03:13 +0200

    Ui fixes

--- laravel/resources/views/layouts/bg-anim.blade.php
+++ laravel/resources/views/layouts/bg-anim.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid col-3 pt-5 pb-3 w-100 d-flex justify-content-center z-1">
+<div class="d-flex container-fluid col-3 pt-5 pb-3 w-100 justify-content-center z-1">
     @if(request()->is('projects'))
         <h2>Some of my projects<span class="blinking-underscore">_</span></h2>
     @else
