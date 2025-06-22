b8b49d01fe22e6256182c004c7baf3edd40f8876
Kovács Ferenc <83652379+Ferref@users.noreply.github.com>
2025-06-22 20:22:38 +0200

    UI fixes

--- laravel/resources/views/projects.blade.php
+++ laravel/resources/views/projects.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="project-container container-fluid d-flex flex-column flex-lg-row gap-4 py-5 p-0">
-    <div class="card-container container-fluid h-50 d-flex flex-row gap-3 flex-column flex-md-row p-0">
+<div class="project-container container-fluid d-flex flex-column flex-lg-row py-5 w-100 p-0 m-0">
+    <div class="card-container row justify-content-center g-4 w-100 p-0 m-0">
         @if(!empty($projects))
             @foreach($projects as $project)
-                <div class="card card-border col-12 col-lg-6 col-xl-4 p-3">
-                <img src="{{ asset('images/projects/' . $project->img_path) }}" class="card-img-top" alt="{{ $project['name'] }}">
-                <div class="card-body d-flex flex-column justify-content-between">
-                    <h5 class="card-title">{{ $project['name'] }}</h5>
-                    <p class="card-text">{{ $project['description'] }}</p>
-                    <a href="{{ $project['link'] }}" class="btn btn-dark">Go to project</a>
-                </div>
+                <div class="col-12 col-sm-6 col-md-4 col-xl-3">
+                    <div class="card h-100 card-border">
+                        <img src="{{ asset('images/projects/' . $project->img_path) }}" class="card-img-top" alt="{{ $project['name'] }}">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <h5 class="card-title">{{ $project['name'] }}</h5>
+                            <p class="card-text">{{ $project['description'] }}</p>
+                            <a href="{{ $project['link'] }}" class="btn btn-dark">Go to project</a>
+                        </div>
+                    </div>
                 </div>
             @endforeach
         @endif
